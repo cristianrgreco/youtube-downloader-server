e532956d911a53973229e11f04993244142b75be
Cristian Greco <cristianrgreco@gmail.com>
2018-02-17 21:40:21 +0000

    Change log level to debug

--- src/controllers.js
+++ src/controllers.js
@@ -3,7 +3,7 @@ const {rabbit: {queueName}} = require('./conf')
 
 const enqueueRequest = async (request, rabbitChannel) => {
   const url = request.params.url
-  logger.info(`enqueueing  url: ${url}`)
+  logger.info(`enqueueing url: ${url}`)
   await rabbitChannel.sendToQueue(queueName, Buffer.from(url))
   return 200
 }
