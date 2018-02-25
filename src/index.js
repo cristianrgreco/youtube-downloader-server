9c634dbce0ecb966ee620e48115e03cc58d22d06
Cristian Greco <cristianrgreco@gmail.com>
2018-02-25 00:31:47 +0000

    Publish and listen on queue using socket IO

--- src/index.js
+++ src/index.js
@@ -1,23 +1,75 @@
 const Hapi = require('hapi')
+const amqplib = require('amqplib')
+const socket = require('socket.io')
 const {logger} = require('./logger')
-const {createChannel} = require('./rabbit')
-const {createRequest} = require('./controllers')
-const {server: {port, host}} = require('./conf');
+const {server: {port, host}} = require('./conf')
+const {rabbit: {url: rabbitUrl, reconnectDelay: rabbitReconnectDelay}} = require('./conf')
+
+const startServer = async () => {
+  logger.info('starting http server', {host, port})
+  const server = new Hapi.Server({port, host, routes: {cors: true}})
+  await server.start()
+  return server
+}
+
+const connectToRabbit = async () => {
+  logger.info('connecting to rabbit', {rabbitUrl})
+
+  return new Promise(resolve => {
+    const connect = async (attempt = 1) => {
+      try {
+        logger.debug('connecting to rabbit', {attempt})
+        resolve(await amqplib.connect(rabbitUrl))
+      } catch (e) {
+        setTimeout(() => connect(attempt + 1), rabbitReconnectDelay)
+      }
+    }
+    return connect()
+  })
+}
+
+const publishRequest = async (rabbit, {url, type}) => {
+  const requestsChannel = await rabbit.createChannel()
+  const requestsQueue = 'requests'
+
+  requestsChannel.assertQueue(requestsQueue, {durable: true})
+  requestsChannel.sendToQueue(
+    requestsQueue,
+    Buffer.from(JSON.stringify({url, type})),
+    {persistent: true}
+  )
+}
+
+const consumeResponses = async (socketClient, rabbit, {url, type}) => {
+  const responseChannel = await rabbit.createChannel()
+  const responseExchange = 'responses'
+  const key = `${Buffer.from(url).toString('base64')}.${type}}`
+
+  responseChannel.assertExchange(responseExchange, 'topic', {durable: false})
+  const {queue: responseQueue} = await responseChannel.assertQueue('', {exclusive: true})
+  responseChannel.bindQueue(responseQueue, responseExchange, key)
+
+  logger.info('waiting for response', {responseQueue, key})
+  responseChannel.consume(responseQueue, message => {
+    const messageContent = JSON.parse(message.content.toString())
+    logger.debug('message received', {messageContent})
+
+    socketClient.emit(key, messageContent)
+  })
+}
 
 (async () => {
-  logger.log('info', 'connecting to rabbit')
-  const rabbitChannel = await createChannel()
-  logger.log('info', 'connected to rabbit')
+  const server = await startServer()
+  const rabbit = await connectToRabbit()
 
-  logger.log('info', 'starting http server', {host, port})
-  const server = new Hapi.Server({port, host, routes: {cors: true}})
-
-  server.route({
-    method: 'POST',
-    path: '/request',
-    handler: request => createRequest(request, rabbitChannel)
+  const io = socket.listen(server.listener)
+  io.on('connection', client => {
+    client.on('request', async ({url, type}) => {
+      logger.info('enqueueing request', {url, type})
+      await publishRequest(rabbit, {url, type})
+      await consumeResponses(client, rabbit, {url, type})
+    })
   })
 
-  await server.start()
-  logger.log('info', 'started http server')
+  logger.log('info', 'ready')
 })()
